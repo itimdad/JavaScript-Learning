0260567933bb2710369311f271286b8e15cf1a6a
itimdad <iamfirdausi01@gmail.com>
2025-10-07 10:01:46 +0000

    learning data types in js

--- 01_basics/01_dataTypes.js
+++ 01_basics/01_dataTypes.js
@@ -1,3 +1,5 @@
+//Primitive Data Typess..................
+
 //undefined
 let a ;
 console.log(a);
@@ -23,3 +25,27 @@ var e = true;
 console.log(e)
 console.log(typeof e);
 
+
+//Complex Data Type;
+
+//Array
+var arr = [2,3,8,4,5,6];
+console.log(arr)
+console.log(arr[4]);
+
+//object
+var person = {
+    id : 102,
+    name: "Imdad",
+    age: 22
+}
+console.log(person)
+//if want to access age from person object
+console.log("age is: ",person.age)
+
+//function
+function hello(){
+    console.log("Hello world")
+}
+
+hello()
