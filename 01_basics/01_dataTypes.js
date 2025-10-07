bfd395047fb4effba534c639b60e010200220bf3
itimdad <iamfirdausi01@gmail.com>
2025-10-07 09:21:21 +0000

    learned data type

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,25 @@
+//undefined
+let a ;
+console.log(a);
+console.log(typeof a);
+
+//String
+let b = "hi"
+console.log(b)
+console.log(typeof b);
+
+//Number
+let c = 4
+console.log(c)
+console.log(typeof c);
+
+//Null
+var d = null;
+console.log(d)
+console.log(typeof d);
+
+//boolean
+var e = true;
+console.log(e)
+console.log(typeof e);
+
